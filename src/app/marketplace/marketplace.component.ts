09ca6285d1dab58cd58b1a28dd2e7f6db4af257c
Safiy Cham <csafi13@yahoo.com>
2017-11-02 10:57:21 -0700

    add album model for marketplace component to import.

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from '../album.model';
 
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
   styleUrls: ['./marketplace.component.css']
 })
-export class MarketplaceComponent implements OnInit {
 
+export class MarketplaceComponent implements OnInit {
+  albums: Album[] = [
+    new Album("Pulse", "Pink Floyd", "A live album by the English progressive rock band.", 1),
+    new Album("Funhouse", "The Stooges", "The second album from an American rock band.", 2)
+  ];
   constructor() { }
 
   ngOnInit() {
